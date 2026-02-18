b327de971e4f36b62c120d05b2a6085643af787f
COMX <COMX@LAPTOP-9F38VMUQ>
2026-02-18 11:47:52 +0200

    My initial commit and added print statements

---
+++ JavaApplication2/src/javaapplication2/JavaApplication2.java
@@ -0,0 +1,15 @@
+
+package javaapplication2;
+
+
+public class JavaApplication2 {
+
+
+    public static void main(String[] args) {
+        System.out.println("My name is Edwin");
+        System.out.println("I made the Mimic");
+        System.out.println("It was difficult to put the pieces together");
+        System.out.println("But unfortunately, something went so wrong");
+    }
+    
+}
